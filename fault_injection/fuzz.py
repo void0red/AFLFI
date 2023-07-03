5678be567b5076117e04da44f66852bc836b93c6
void0red <void0red@gmail.com>
2023-07-03 17:29:14 +0800

    fuzz cov support
    Signed-off-by: void0red <void0red@gmail.com>

--- fault_injection/fuzz.py
+++ fault_injection/fuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import time
 from argparse import ArgumentParser
 from pathlib import Path
 from runner import RunnerPool
@@ -11,8 +12,9 @@ def read_cmd(fn):
         return f.read().splitlines()
 
 
-def concat_cmd(instance):
+def concat_cmd(instance, new_bin):
     cmds_ = read_cmd(instance / 'cmdline')
+    cmds_[0] = new_bin
     # current not support stdin input
     assert '@@' in cmds_
     idx = cmds_.index('@@')
@@ -25,27 +27,31 @@ def concat_cmd(instance):
     return ret
 
 
-def get_all_cmd(out):
+def get_all_cmd(out, new_bin):
     ret = set()
     for i in os.listdir(out):
-        ret.update(concat_cmd(Path(out).joinpath(i)))
+        ret.update(concat_cmd(out / i, new_bin))
     return ret
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('out', type=str, help='afl fuzz output')
+    parser.add_argument('bin', type=str, help='target bin with cov inst')
     sys_args = parser.parse_args()
     assert Path(sys_args.out).exists()
 
     pool = RunnerPool(os.cpu_count(), 30)
     fuzzed = set()
+    start = time.time()
     while True:
-        cmds = get_all_cmd(sys_args.out) - fuzzed
+        cmds = get_all_cmd(sys_args.out, sys_args.bin) - fuzzed
         fuzzed.update(cmds)
-        if len(cmds) > 0:
-            print(f'fetch {len(cmds)} new testcase')
+        if len(cmds) == 0:
+            os.sched_yield()
+            continue
+        print(f'\nfetch {len(cmds)} new testcase')
         loop = asyncio.new_event_loop()
         for cmd in cmds:
-            print(cmd)
             loop.run_until_complete(pool.run_cmd(False, False, *cmd.split()))
+        print(f'\ncost {time.time() - start}s')
